00ebf5ec068250a613d28a9971377f042d5089b7
MoisesBaron1 <162084944+MoisesBaron1@users.noreply.github.com>
2024-05-03 17:26:10 -0500

    Create emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } } 
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    else:
+        emotion = data['emotionPredictions'][0]['emotion']
+        anger_score = emotion['anger']
+        disgust_score = emotion['disgust']
+        fear_score = emotion['fear']
+        joy_score = emotion['joy']
+        sadness_score = emotion['sadness']
+        dominant_emotion = max(emotion, key=emotion.get)
+        return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+        }
